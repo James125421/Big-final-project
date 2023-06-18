5cf642bde1c6d4185111c04a2670af2a8b691195
James125421 <james1254219999@gmail.com>
2023-06-19 02:21:54 +0800

    add minimax function

---
+++ src/policy/minimax.cpp
@@ -0,0 +1,54 @@
+#include <cstdlib>
+#include <set>
+#include "../state/state.hpp"
+#include "./minimax.hpp"
+
+
+/**
+ * @brief Randomly get a legal action
+ * 
+ * @param state Now state
+ * @param depth You may need this for other policy
+ * @return Move 
+ */
+Move Minimax::get_move(State *state, int depth){
+    int Value=-1e10;
+    if(!state->legal_actions.size())
+        state->get_legal_actions();
+  
+    auto actions = state->legal_actions;
+    int id=0;
+    int ans=id;
+    for(auto it=actions.begin();it!=actions.end();it++){
+        int tmpValue=get_value(state->next_state(*it),depth,false);
+        if(Value <= tmpValue){
+            Value=tmpValue;
+            ans=id;
+        }
+        id++;
+    }
+    return actions[ans];
+    //return actions[(rand()+depth)%actions.size()];
+}
+
+int Minimax::get_value(State *state, int depth,bool player){
+    int value;
+    if(!depth || !state->legal_actions.size())
+        return state->evaluate();
+    if(player){
+        value=-1e10;
+        auto actions = state->legal_actions;
+        for(auto it=actions.begin();it!=actions.end();it++){
+            value=std::max(value,get_value(state->next_state(*it),depth-1,false));
+        }
+        return value;
+    }
+    else{
+        value=1e10;
+        auto actions = state->legal_actions;
+        for(auto it=actions.begin();it!=actions.end();it++){
+            value=std::min(value,get_value(state->next_state(*it),depth-1,true));
+        }
+        return value;
+    }
+}

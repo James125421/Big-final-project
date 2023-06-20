314fa8a691135ab1c810e16dc57495ce4563fee2
James125421 <james1254219999@gmail.com>
2023-06-20 17:00:33 +0800

    add 10 second

--- src/player/alpha_beta.cpp
+++ src/player/alpha_beta.cpp
@@ -27,16 +27,18 @@ void read_board(std::ifstream& fin) {
 
 void write_valid_spot(std::ofstream& fout) {
   // Keep updating the output until getting killed.
+  int depth=1;
   while(true) {
-    // Choose a random spot.
-    auto move = Alpha_beta::get_move(root, 5);
-    fout << move.first.first << " " << move.first.second << " "\
-         << move.second.first << " " << move.second.second << std::endl;
-    
-    // Remember to flush the output to ensure the last action is written to file.
+    while(true){
+      auto move = Alpha_beta::get_move(root, depth);
+      fout << move.first.first << " " << move.first.second << " "\
+          << move.second.first << " " << move.second.second << std::endl;
+    }
     fout.flush();
+    depth++;
     break;
   }
+  
 }
 
 int main(int, char** argv) {
